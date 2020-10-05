ca8ecd438e4c1455c21f5344f591e763f682986b
JOHN NASIMBA <nasimba4john@gmail.com>
2020-10-05 15:40:11 +0200

    Added react hooks

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList'
 import NotFoundPage from './notFoundPage';
 
 
 const ArticlePage = ({ match }) => {
+
     const name = match.params.name
     const article = articleContent.find(article => article.name === name)
+
+    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
+    useEffect(() => {
+        setArticleInfo({upvotes: 6})
+    }, [])
     if (!article) return <NotFoundPage/>
     const otherArticles = articleContent.filter(article => article.name !== name);
      return ( 
          <div>
              <h1>{article.title} </h1>
+             <p>This post have been upvoted {articleInfo.upvotes} times</p>
              {article.content.map((paragraph, key) => (
                  <p key={key}> {paragraph} </p>
              ))}
